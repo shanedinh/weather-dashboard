9147bfb7be0d90c480470529c1a33d559d2f3114
Shane Dinh <shane.dinh5@gmail.com>
2022-06-12 13:13:04 -0500

    5 day forecast functioning and displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,38 @@ var apiKey = "9294f4f11ff92a5fdbc25e0f6665381e";
 var mainWeather = document.getElementById('weatherResults');
 var mainBorder = document.getElementById('mainBorder');
 var currentUvEl = document.getElementById('uv-text')
+var forecastEl = document.getElementById('forecast');
+var historyEl = document.getElementById('history');
+var fore5Header = document.getElementById('header5');
+var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
 
 
-// hide border
+// hide elements before search
 mainBorder.style.display = "none";
 
 var getWeather = function() {
 
   //display border after search displays
   mainBorder.style.display = "block";
+  
 
   var cityName = document.getElementById('locationSearch').value;
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=da8f0c321e0385f0aaa59a5f95320076&units=imperial`;
+
+  // searchHistory.push(cityName);
+  // localStorage.setItem("search", JSON.stringify(searchHistory));
+  // var cityList = document.createElement('p');
+  // cityList.textContent = cityName;
+  // historyEl.append(cityList);
+
+  // function showSearchHistory() {
+  //   // historyEl.innerHTML = "";
+  //   for (let i = 0; i < searchHistory.length; i++) {
+  //     var historyItem = document.createElement("p");
+  //     historyItem.textContent = searchHistory[i]
+  //     historyEl.append(historyItem);
+  //   }
+  // }
 
   fetch(url)
   .then(function(response) {
@@ -25,13 +45,13 @@ var getWeather = function() {
 
     var date = moment().format('L');
     var displayDate = document.createElement('h2');
-    displayDate.textContent = data.name + " | " + moment().format('L');
+    displayDate.textContent = data.name + " | " + date;
     mainWeather.appendChild(displayDate);
 
-    var weatherIcon = document.createElement('img');
+    var dailyIcon = document.createElement('img');
     var weatherID = data.weather[0].icon;
-    weatherIcon.src = "https://openweathermap.org/img/wn/" + weatherID + ".png";
-    mainWeather.appendChild(weatherIcon);
+    dailyIcon.src = "https://openweathermap.org/img/wn/" + weatherID + ".png";
+    mainWeather.appendChild(dailyIcon);
 
     var temp = document.createElement('p');
     temp.textContent = "Temp: " + data.main.temp + "°F";
@@ -50,7 +70,7 @@ var getWeather = function() {
     var lon = data.coord.lon;
     var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
-    
+    // fetch UV index and display
     fetch(uvUrl)
     .then(function(response) {
       return response.json();
@@ -78,8 +98,47 @@ var getWeather = function() {
         uvIndex.setAttribute("class", "w-1/3 bg-red-100 text-red-800 text-md font-semibold mt-1.5 mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900");
       }
 
+      // 5 day forecast
+      for (var i = 0; i < data.daily.length - 3; i++) {
+        var forecastCards = document.createElement('div')
+        var weatherCard = document.createElement('div')
+        var card = document.createElement('div')
+
+        var forecastDate5 = document.createElement('h3');
+        forecastDate5.textContent = moment.unix(data.daily[i].dt).format('L');
+        forecastCards.appendChild(forecastDate5);
+
+        var forecastTemp5 = document.createElement('p');
+        forecastTemp5.textContent = "Temperature: " + data.daily[i].temp.day + "°F";
+        forecastEl.appendChild(forecastTemp5);
+
+        var windForecast5 = document.createElement('p');
+        windForecast5.textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
+        forecastEl.appendChild(windForecast5);
+
+        var humidityForecast5 = document.createElement('p');
+        humidityForecast5.textContent = "Humidity: " + data.daily[i].humidity + "%";
+        forecastEl.appendChild(humidityForecast5);
+
+        var icon = document.createElement('img');
+        icon.src = "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+        forecastEl.appendChild(icon);
+
+        card.appendChild(forecastDate5);
+        card.appendChild(icon);
+        card.appendChild(forecastTemp5);
+        card.appendChild(windForecast5);
+        card.appendChild(humidityForecast5);
+        weatherCard.appendChild(card);
+        forecastCards.appendChild(weatherCard);
+        forecastEl.appendChild(weatherCard);
+  
+      }
+
+
     })
   })
 }
 
 searchBtnEl.addEventListener("click", getWeather)
+
