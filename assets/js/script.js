17f8df3cc28bc0dc130d18c4828c35738319a947
Shane Dinh <shane.dinh5@gmail.com>
2022-06-12 19:14:56 -0500

    search history list created after first search

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ var mainBorder = document.getElementById('mainBorder');
 var currentUvEl = document.getElementById('uv-text')
 var forecastEl = document.getElementById('forecast');
 var historyEl = document.getElementById('history');
-var fore5Header = document.getElementById('header5');
+var searchedListEl = document.getElementById('searchedCities');
 var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
+
 
 
 // hide elements before search
@@ -14,27 +15,16 @@ mainBorder.style.display = "none";
 
 var getWeather = function() {
 
+
   //display border after search displays
   mainBorder.style.display = "block";
   
 
   var cityName = document.getElementById('locationSearch').value;
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=da8f0c321e0385f0aaa59a5f95320076&units=imperial`;
-
-  // searchHistory.push(cityName);
-  // localStorage.setItem("search", JSON.stringify(searchHistory));
-  // var cityList = document.createElement('p');
-  // cityList.textContent = cityName;
-  // historyEl.append(cityList);
-
-  // function showSearchHistory() {
-  //   // historyEl.innerHTML = "";
-  //   for (let i = 0; i < searchHistory.length; i++) {
-  //     var historyItem = document.createElement("p");
-  //     historyItem.textContent = searchHistory[i]
-  //     historyEl.append(historyItem);
-  //   }
-  // }
+ 
+ 
+  // main/day of weather
 
   fetch(url)
   .then(function(response) {
@@ -65,12 +55,12 @@ var getWeather = function() {
     humidity.textContent = 'Humidity: ' + data.main.humidity + "%";
     mainWeather.appendChild(humidity);
 
+    // fetch UV index and display
 
     var lat = data.coord.lat;
     var lon = data.coord.lon;
-    var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
 
-    // fetch UV index and display
     fetch(uvUrl)
     .then(function(response) {
       return response.json();
@@ -78,9 +68,8 @@ var getWeather = function() {
     .then(function(data){
       console.log(data);
 
+      // UV Index color coded. Green - low, Red - very high.
       var uvIndex = document.createElement('span');
-      // uvIndex.textContent = 'UV Index: ' + data.current.uvi;
-      // mainWeather.appendChild(uvIndex);
       if (data.current.uvi < 2) {
         uvIndex.setAttribute("class", "w-1/3 bg-green-100 text-green-800 text-md font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900");
       }
@@ -99,21 +88,23 @@ var getWeather = function() {
       }
 
       // 5 day forecast
+      // loop and create 5 day forecast cards
       for (var i = 0; i < data.daily.length - 3; i++) {
-        var forecastCards = document.createElement('div')
-        var weatherCard = document.createElement('div')
         var card = document.createElement('div')
+        card.setAttribute("class", "bg-blue-600 text-white border border-black p-5 rounded-sm m-1")
+
 
         var forecastDate5 = document.createElement('h3');
         forecastDate5.textContent = moment.unix(data.daily[i].dt).format('L');
-        forecastCards.appendChild(forecastDate5);
+        forecastDate5.setAttribute("class", "font-bold");
+        forecastEl.appendChild(forecastDate5);
 
         var forecastTemp5 = document.createElement('p');
         forecastTemp5.textContent = "Temperature: " + data.daily[i].temp.day + "°F";
         forecastEl.appendChild(forecastTemp5);
 
         var windForecast5 = document.createElement('p');
-        windForecast5.textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
+        windForecast5.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
         forecastEl.appendChild(windForecast5);
 
         var humidityForecast5 = document.createElement('p');
@@ -129,16 +120,33 @@ var getWeather = function() {
         card.appendChild(forecastTemp5);
         card.appendChild(windForecast5);
         card.appendChild(humidityForecast5);
-        weatherCard.appendChild(card);
-        forecastCards.appendChild(weatherCard);
-        forecastEl.appendChild(weatherCard);
-  
+        forecastEl.appendChild(card);
       }
 
+      function showSearchHistory() {
+      // save cities to localStorage
+        historyEl = [];
+        for (i = 0; i < searchHistory.length; i++) {
+          localStorage.setItem('search', JSON.stringify(cityName))
+          var savedCity = document.createElement('button')
+          savedCity.textContent = cityName;
+          savedCity.setAttribute("class", "w-full m-2 ml-6 bg-slate-400 rounded text-white text-center px-3 py-1 justify-center")
+          historyEl.push(savedCity)
+          searchedListEl.appendChild(savedCity);
+          savedCity.addEventListener("click", function(){
+            getWeather(savedCity.value);
+          })
+          historyEl.append(savedCity);
+        }
+      }
 
+      showSearchHistory();
+      if (searchHistory.length > 0) {
+        getWeather(searchHistory);
+      }
     })
   })
 }
 
 searchBtnEl.addEventListener("click", getWeather)
-
+localStorage.getItem('search', 'cityName[i]')
